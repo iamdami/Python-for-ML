fab6b12860f00a1404280dd84edc7ba22df5cf1c
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-11 21:42:51 +0900

    NestedForLoop_if.py

--- NestedForLoop_if.py
+++ NestedForLoop_if.py
@@ -1,8 +1,10 @@
+  
 case_1 = ["A", "B", "C"]
 case_2 = ["D", "E", "A"]
 
 result = [i+j for i in case_1 for j in case_2]
 print(result)
+# ['AD', 'AE', 'AA', 'BD', 'BE', 'BA', 'CD', 'CE', 'CA']
 
 
 # add if
@@ -10,3 +12,4 @@ result = [i+j for i in case_1 for j in case_2 if not(i==j)]
 result.sort()
 
 print(result)
+# ['AD', 'AE', 'BA', 'BD', 'BE', 'CA', 'CD', 'CE']
