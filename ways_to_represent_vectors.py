38547a48fcd370178a9833693bed0feef58f4180
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-15 19:14:25 +0900

    Create ways_to_represent_vectors.py

---
+++ ways_to_represent_vectors.py
@@ -0,0 +1,7 @@
+# There are various ways to represent vectors in Python
+
+vector_a = [1, 2, 10]
+vector_b = (1, 2, 10)
+vector_c = {'x':1, 'y':1, 'x':10}
+print(vector_a, vector_b, vector_c)
+# [1, 2, 10] (1, 2, 10) {'x': 10, 'y': 1}
