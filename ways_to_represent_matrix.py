cff520f10ca103c65a297c82c478d5d67711f04f
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-01-15 19:20:38 +0900

    Create ways_to_represent_matrix.py

---
+++ ways_to_represent_matrix.py
@@ -0,0 +1,7 @@
+# Matrix representation
+
+matrix_a = [[3,6], [4,5]]
+matrix_b = [(3,6), (4,5)]
+matrix_c = {(0,0): 3, (0,1): 6, (1,0): 4, (1,1): 5}
+print(matrix_a, matrix_b, matrix_c)
+# [[3, 6], [4, 5]] [(3, 6), (4, 5)] {(0, 0): 3, (0, 1): 6, (1, 0): 4, (1, 1): 5}
