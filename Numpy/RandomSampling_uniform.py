99755e6d67a2a69936a1c2db076c2a299cf32dfa
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-02-18 00:06:10 +0900

    Create RandomSampling_uniform.py

---
+++ Numpy/RandomSampling_uniform.py
@@ -0,0 +1,6 @@
+# Create array by sampling according to data distribution
+import numpy as np
+
+np.random.uniform(0,1,10).reshape(2,5)
+"""[[ 0.67406593,  0.71072857,  0.06963986,  0.09194939,  0.47293574],
+       [ 0.13840676,  0.97410297,  0.60703044,  0.04002073,  0.08057727]]"""
