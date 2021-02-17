2a07d2f32fa51b8408dd2513e3db47c6abb4b67b
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-02-17 23:53:57 +0900

    Create array_creation.py

---
+++ Numpy/array_creation.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+test = np.array([1, 4, 5, 8], float)
+print(test)
+print(type(test[3]))
+
+
+"""
+output
+[1. 4. 5. 8.]
+<class 'numpy.float64'>
+# Memory space occupied by one element is 64 bits
+"""
