46d385b324c8450e81f350d2f68e29488a81537d
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-02-17 23:57:34 +0900

    Create indexing.py
    -Unlike List, notation such as [0,0] is provided in a two-dimensional array.
    -In case of Matrix, the front means row and the back means column.

---
+++ Numpy/indexing.py
@@ -0,0 +1,7 @@
+import numpy as np
+test_exmaple = np.array([[1, 2, 3], [4.5, 5, 6]], int)
+print(test_exmaple)
+"""[[1 2 3]
+   [4 5 6]]"""
+print(test_exmaple[0][0])  # 1
+print(test_exmaple[0,0])  # 1
