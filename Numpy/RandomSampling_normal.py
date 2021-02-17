acaae049e66289cb588d32f57463e8e9478353bc
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-02-18 00:07:03 +0900

    Create RandomSampling_normal.py

---
+++ Numpy/RandomSampling_normal.py
@@ -0,0 +1,6 @@
+# Create array by sampling according to data distribution
+import numpy as np
+
+np.random.normal(0,1,10).reshape(2,5)
+"""[[ 1.02694847,  0.39354215,  0.63411928, -1.03639086, -1.76669162],
+       [ 0.50628853, -1.42496802,  1.23288754,  1.26424168,  0.53718751]]"""
