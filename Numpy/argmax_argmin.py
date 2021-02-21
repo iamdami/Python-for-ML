4e80cf651c85eeb18da907bd317bf5ea87c674bb
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-02-21 12:19:35 +0900

    Create argmax_argmin.py

---
+++ Numpy/argmax_argmin.py
@@ -0,0 +1,10 @@
+# Returns the index number of the maximum or minimum value among array indexes
+
+import numpy as np 
+
+a = np.array([1, 2, 4, 5, 8, 78, 23, 3])
+print(np.argmax(a), np.argmin(a))
+
+"""
+5 0
+"""
