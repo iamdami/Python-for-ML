cb80db74e8317e9ce1e2b59ac8d58bf7e67b3330
Dami Lee <50016477+iamdami@users.noreply.github.com>
2021-02-27 19:04:08 +0900

    Create Transpose_PrintValues_ToCsv_DeleteCol.py

---
+++ Pandas/DataFrame/Transpose_PrintValues_ToCsv_DeleteCol.py
@@ -0,0 +1,19 @@
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+
+
+# Transpose
+df.T
+
+
+# Print Value
+df.values
+
+
+# convert to csv file
+df.to_csv()
+
+
+# Delete Column
+del df["debt"]
