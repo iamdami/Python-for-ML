22db6d726853f8c067188b8abf418f6031ba6932
Dami Lee <50016477+iamdami@users.noreply.github.com>
2022-04-15 10:35:44 +0900

    Update concatenate_axis_1.py

--- Numpy/concatenate_axis_1.py
+++ Numpy/concatenate_axis_1.py
@@ -2,6 +2,9 @@ import numpy as np
 
 test_a = np.array([[1, 2], [3, 4]]) 
 test_b = np.array([[5, 6]]) 
+# all the input array dimensions for the concatenation axis must match exactly  
+# test_b = np.array([[5, 6, 7]]) 이런 거 안된다는 거!  
+
 print(np.concatenate((test_a, test_b.T), axis=1))
 
 """
